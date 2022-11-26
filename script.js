7b5db4238cd39112ee0c208c746d579fb6448696
TibiaBones <78929148+TibiaBones@users.noreply.github.com>
2022-11-26 19:38:09 +0300

    add the function of opening and closing the date settings menu

---
+++ script.js
@@ -0,0 +1,15 @@
+function openMenu() {
+    if(document.getElementById('menuPickDate').style.display == 'flex') {
+        document.getElementById('menuPickDate').style.display = 'none'
+        document.getElementById('menuPickDate-background').style.display = 'none';
+        console.log('close')} else {
+            document.getElementById('menuPickDate').style.display = 'flex';
+            document.getElementById('menuPickDate-background').style.display = 'block';
+            console.log('open');
+    };
+};
+
+function closeMenu() {
+    document.getElementById('menuPickDate').style.display = 'none';
+    document.getElementById('menuPickDate-background').style.display = 'none';
+}
