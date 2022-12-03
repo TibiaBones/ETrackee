aeada8e7fd756137953881ff12e19deb313a780a
TibiaBones <78929148+TibiaBones@users.noreply.github.com>
2022-12-03 14:42:03 +0300

    add datepicker

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-function openMenu() {
-    if(document.getElementById('menuPickDate').style.display == 'flex') {
-        document.getElementById('menuPickDate').style.display = 'none'
-        document.getElementById('menuPickDate-background').style.display = 'none';
-        console.log('close')} else {
-            document.getElementById('menuPickDate').style.display = 'flex';
-            document.getElementById('menuPickDate-background').style.display = 'block';
-            console.log('open');
-    };
-};
+// function openMenu() {
+//     if(document.getElementById('menuPickDate').style.display == 'flex') {
+//         document.getElementById('menuPickDate').style.display = 'none'
+//         document.getElementById('menuPickDate-background').style.display = 'none';
+//         console.log('close')} else {
+//             document.getElementById('menuPickDate').style.display = 'flex';
+//             document.getElementById('menuPickDate-background').style.display = 'block';
+//             console.log('open');
+//     };
+// };
 
-function closeMenu() {
-    document.getElementById('menuPickDate').style.display = 'none';
-    document.getElementById('menuPickDate-background').style.display = 'none';
-}
+// function closeMenu() {
+//     document.getElementById('menuPickDate').style.display = 'none';
+//     document.getElementById('menuPickDate-background').style.display = 'none';
+// }
+
+new AirDatepicker('#datepicker');
