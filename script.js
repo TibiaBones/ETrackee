25f2727c2eea9ec5bfca1b5e26e80d2a1fd2d4af
TibiaBones <78929148+TibiaBones@users.noreply.github.com>
2022-12-04 19:10:49 +0300

    pre-add scripts for calculating the amount of wages

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-// function openMenu() {
-//     if(document.getElementById('menuPickDate').style.display == 'flex') {
-//         document.getElementById('menuPickDate').style.display = 'none'
-//         document.getElementById('menuPickDate-background').style.display = 'none';
-//         console.log('close')} else {
-//             document.getElementById('menuPickDate').style.display = 'flex';
-//             document.getElementById('menuPickDate-background').style.display = 'block';
-//             console.log('open');
-//     };
-// };
-
-// function closeMenu() {
-//     document.getElementById('menuPickDate').style.display = 'none';
-//     document.getElementById('menuPickDate-background').style.display = 'none';
-// }
-
 new AirDatepicker('#datepicker', {
     position: 'bottom center'
 });
+
+const status25Price = 0.0344;
+const status28Price = 0.043;
+const status30Price = 0;
+const status40Price = 0.07;
+const entityPrice = 0.05;
+// уточнить стоимость операций
+
+function calc25(){
+    let statusValue25 = document.getElementById('calcInput-textAarea25').value; 
+    let result = status25Price * statusValue25;
+    document.getElementById('result25').innerHTML = result;
+};
+
+function calc28(){
+    let statusValue28 = document.getElementById('calcInput-textAarea25').value; 
+    let result = status28Price * statusValue28;
+    document.getElementById('result28').innerHTML = result;
+};
+
+function calc30(){
+    let statusValue30 = document.getElementById('calcInput-textAarea25').value; 
+    let result = status30Price * statusValue30;
+    document.getElementById('result30').innerHTML = result;
+};
+
+function calc40(){
+    let statusValue40 = document.getElementById('calcInput-textAarea25').value; 
+    let result = status40Price * statusValue40;
+    document.getElementById('result40').innerHTML = result;
+};
+
+function getResult(){
+    let result25 = document.getElementById('result25').textContent;
+    let result28 = document.getElementById('result28').textContent;
+    let result30 = document.getElementById('result30').textContent;
+    let result40 = document.getElementById('result40').textContent;
+    let sum = +(result25 + result28 + result30 + result40);
+    document.getElementById('calcResult').innerHTML = sum;
+    console.log(sum);
+};
